07758454e5d9bfc76668a402a290918a99cf4dad
Youssed Darwish <youssefdarwish95@gmail.com>
2018-11-28 10:19:59 +0200

    Resolving conflicts :'S

--- client/client.c
+++ client/client.c
@@ -1,247 +1,3 @@
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-  
-#define IP_PROTOCOL 0 
-#define IP_ADDRESS "127.0.0.1" // localhost 
-#define PORT_NO 15050 
-#define NET_BUF_SIZE 500
-#define sendrecvflag 0 
-  
-int counter;
-
-
-
-struct data_packet {
-/* Header */
-    uint16_t cksum; /* Optional bonus part */
-    uint16_t len;
-    uint32_t seqno;
-    time_t sent_time;
-    /* Data */
-    char data[500]; 
-};
-
-/* Ack-only packets are only 8 bytes */
-struct ack_packet {
-    uint16_t cksum; /* Optional bonus part */
-    uint16_t len;
-    uint32_t ackno;
-    uint32_t seqno;
-
-};
-
-struct data_packet get_packet(char *data){
-    
-    struct data_packet packet;
-    packet.seqno = counter;
-    counter++;
-    packet.sent_time = time(NULL);
-    strcpy(packet.data, data);
-    packet.len = sizeof(packet.data);
-    return packet;
-}
-
-
-// funtion to clear buffer 
-void clearBuf(char* b) 
-{ 
-    int i; 
-    for (i = 0; i < NET_BUF_SIZE; i++) 
-        b[i] = '\0'; 
-} 
-  
-// function to receive file 
-int recvFile(char* buf, int s) 
-{ 
-    int i; 
-    char ch; 
-    for (i = 0; i < s; i++) { 
-        ch = buf[i]; 
-        if (ch == EOF) 
-            return 1; 
-        else
-            printf("%c", ch); 
-    } 
-    return 0; 
-} 
-  
-// driver code 
-int main() 
-{ 
-    int sockfd, nBytes; 
-    struct data_packet packet;
-    packet.seqno=1212;
-    packet.sent_time = time(NULL);
-
-    struct sockaddr_in addr_con; 
-    int addrlen = sizeof(addr_con); 
-    addr_con.sin_family = AF_INET; 
-    addr_con.sin_port = htons(PORT_NO); 
-    addr_con.sin_addr.s_addr = inet_addr(IP_ADDRESS); 
-    char net_buf[sizeof(packet)]; 
-    FILE* fp; 
-    // socket() 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 
-                    IP_PROTOCOL); 
-  
-    if (sockfd < 0) 
-        printf("\nfile descriptor not received!!\n"); 
-    else
-        printf("\nfile descriptor %d received\n", sockfd); 
-    while (1) { 
-        printf("\nPlease enter file name to receive:\n"); 
-
-        scanf("%s",packet.data);
-        packet.sent_time = time(NULL);
-        memcpy(net_buf,&packet.seqno,sizeof (packet.seqno));
-        memcpy(net_buf+sizeof (packet.seqno),&packet.sent_time,sizeof (packet.sent_time));
-        memcpy(net_buf+2*sizeof(int),&packet.data ,sizeof (packet.data));
-
-        sendto(sockfd, net_buf, NET_BUF_SIZE, 
-               sendrecvflag, (struct sockaddr*)&addr_con, 
-               addrlen); 
-  
-        printf("\n---------Data Received---------\n"); 
-
-        struct ack_packet ack;
-        char buff_recv [sizeof(packet)];
-        char buffer [sizeof ( ack)];
-        struct data_packet my_packet;
-        while (1) { 
-            // receive 
-            clearBuf(buff_recv); 
-            nBytes = recvfrom(sockfd, buff_recv, sizeof (buff_recv), 
-                              sendrecvflag, (struct sockaddr*)&addr_con, 
-                              &addrlen); 
-
-            // process
-            memcpy(&my_packet.seqno,buff_recv ,sizeof(int));
-            memcpy(&my_packet.sent_time,buff_recv +sizeof(int),sizeof(int));
-            memcpy(&my_packet.data,buff_recv + 2*sizeof (int)
-            ,sizeof(my_packet.data));
-            printf("\nseq ,time %d %d\n",my_packet.seqno,my_packet.sent_time);
-
-            if (recvFile(buff_recv+2*sizeof (int), NET_BUF_SIZE)) { 
-                break; 
-            } 
-            //clear buffer
-
-
-            clearBuf(buffer);
-            //sending ACK
-            ack.seqno = my_packet.seqno;
-            ack.ackno = my_packet.seqno;
-
-            memcpy(buffer,&ack.seqno,sizeof (ack.seqno));
-            memcpy(buffer+sizeof (ack.seqno),&ack.ackno,sizeof (ack.ackno));
-
-            sendto(sockfd, buffer, 2 * sizeof (ack.ackno), 
-               sendrecvflag, (struct sockaddr*)&addr_con, 
-               addrlen); 
-
-            printf("\n\n Ack Sent : %d\n",ack.ackno);
-
-
-            
-        } 
-        printf("\n-------------------------------\n"); 
-    } 
-    return 0; 
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-#define MAXLEN 100
-#define NET_BUF_SIZE 32 
-
-int counter;
-struct data_packet {
-
-
-
-void recv_stop_and_wait (char * fileName,int sockfd , struct* sockaddr_in addr_con){
-    
-    counter = rand() %10;
-    struct data_packet packet = get_packet(fileName);
-    char buff [MAXLEN];
-
-    memcpy (buff,&packet,sizeof(packet));
-
-    //time out
-    sendto(sockfd, buff,sizeof(fileName), 
-            0, (struct sockaddr*)&addr_con, 
-            sizeof(addr_con)); 
-    
-    // 
-    printf("\n---------Data Received---------\n"); 
-
-    while (1) { 
-        // receive  
-        int nBytes = recvfrom(sockfd, net_buf, NET_BUF_SIZE, 
-                            0, (struct sockaddr*)&addr_con, 
-                            &addrlen); 
-
-        // process 
-        if (recvFile(net_buf, NET_BUF_SIZE)) { 
-            break; 
-        } 
-    } 
-    printf("\n-------------------------------\n"); 
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -252,23 +8,22 @@ void recv_stop_and_wait (char * fileName,int sockfd , struct* sockaddr_in addr_c
 #include <unistd.h>
 
 
-#define MAXLEN 100
+#define MAX_LEN 500
 
 int counter;
 struct data_packet {
-    /* Header 
-    uint16_t cksum; /* Optional bonus part 
+    /* Header */
+    uint16_t cksum; /* Optional bonus part */
     uint16_t len;
     uint32_t seqno;
     time_t sent_time;
-    /* Data 
-    char data[500]; 
+    /* Data */
+    char data[500]; /* Not always 500 bytes, can be less */
 };
 
-
 struct ack_packet {
-    /* Header 
-    uint16_t cksum; /* Optional bonus part 
+    /* Header */
+    uint16_t cksum; /* Optional bonus part */
     uint16_t len;
     uint32_t seqno;
     time_t sent_time;
@@ -285,54 +40,40 @@ struct data_packet get_packet(char data[]){
     return packet;
 }
 
-void stop_and_wait(char file_name[], int sock_fd, struct sockaddr_in * addr_con){
+void stop_and_wait(char file_name[], int sock_fd, struct sockaddr_in addr_con){
 
     counter = rand() %10;
     struct data_packet packet = get_packet(file_name);
-    char buff[MAXLEN];
+    char buff[MAX_LEN];
+    socklen_t socklen = sizeof(addr_con);
 
-    memcpy (buff, &packet, sizeof(packet));
+    if(sendto(sock_fd, (const void *) &packet, sizeof(struct data_packet), 0,
+             (struct sockaddr *) &addr_con, socklen) == -1)
+    {
+        fprintf(stderr, "send() failed.\n");
+        return;
+    }
 
-    //time out
-    sendto(sock_fd, &buff, sizeof(buff), MSG_CONFIRM, (struct sockaddr*) &addr_con, sizeof(addr_con));
-    puts("msg sent");
+    FILE *file = fopen(file_name, "w");
+    if (file == NULL) {
+        fprintf(stderr, "File not found\n");
+        return;
+    }
 
-
-    // while (1) {
-    //     // receive
-    //     nBytes = recvfrom(sockfd, net_buf, NET_BUF_SIZE,
-    //                         sendrecvflag, (struct sockaddr*)&addr_con,
-    //                         &addrlen);
-
-    //     // process
-    //     if (recvFile(net_buf, NET_BUF_SIZE)) {
-    //         break;
-    //     }
-    // }
-    // printf("\n-------------------------------\n");
-
-
+    //while (1) {
+        // receive
+        while(recvfrom(sock_fd, (void *) &packet, sizeof(struct data_packet),
+                   0, (struct sockaddr *) &addr_con, &socklen) > 0){
+                        fputs(packet.data, file);
+                   }
+                puts(packet.data);
+    //}
 
 }
 
 
-
-
 int main(int argc, char const *argv[])
 {
-    /* code */
-
-    // check args , get args     int sockfd;
-
-    // read inputs &args from file
-
-    /*
-    IP address of server.
-Well-known port number of server.
-Port number of client.
-Filename to be transferred (should be a large file).
-Initial receiving sliding-window size (in datagram units).
-    
 
     FILE *file = fopen("client.in", "r");
     if (file == NULL) {
@@ -354,26 +95,17 @@ Initial receiving sliding-window size (in datagram units).
 
     //printf("%s %s %s %s %d", ip_addr, server_port_no, client_port_no, file_name, window_size);
 
-     int socket_fd = socket(AF_INET,SOCK_DGRAM, 0);
-     if (socket_fd <0)
-         puts("error");
+    int socket_fd = socket(AF_INET,SOCK_DGRAM, 0);
+    if (socket_fd <0)
+        puts("error");
 
-     struct sockaddr_in server_addr;
-     memset(&server_addr, 0, sizeof(server_addr));
-     server_addr.sin_family = AF_INET;
-     server_addr.sin_port = htons(server_port_no);
-     server_addr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port_no);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-
-     //TEST CONNECTION
-
-     //char *hello = "hello from client";
-     //sendto(socket_fd, (char *)hello, strlen(hello), MSG_CONFIRM, (struct sockaddr *) &server_addr, sizeof(server_addr));
-
-     //printf("hello message sent\n");
-
-     stop_and_wait(file_name, socket_fd , &server_addr);
+    stop_and_wait(file_name, socket_fd , server_addr);
 
     return 0;
 }
-*/
--- server/server.c
+++ server/server.c
@@ -1,45 +1,38 @@
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-  
-#define IP_PROTOCOL 0 
-#define PORT_NO 15050 
-#define NET_BUF_SIZE 500 
-#define sendrecvflag 0 
-#define nofile "File Not Found!" 
-  
-int counter;  
-int header_size;
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+#define MAX_LEN 500
 
+#ifndef min
+#define min(a, b)            (((a) < (b)) ? (a) : (b))
+#endif
 
-
-
-
+int counter;
 struct data_packet {
-/* Header */
-uint16_t cksum; /* Optional bonus part */
-uint16_t len;
-uint32_t seqno;
-time_t sent_time;
-/* Data */
-char data[500]; /* Not always 500 bytes, can be less */
-};
-
-
-/* Ack-only packets are only 8 bytes */
-struct ack_packet {
+    /* Header */
     uint16_t cksum; /* Optional bonus part */
     uint16_t len;
-    uint32_t ackno;
     uint32_t seqno;
-
+    time_t sent_time;
+    /* Data */
+    char data[500]; /* Not always 500 bytes, can be less */
 };
+
+
+struct ack_packet {
+    /* Header */
+    uint16_t cksum; /* Optional bonus part */
+    uint16_t len;
+    uint32_t seqno;
+    time_t sent_time;
+};
+
 
 struct data_packet get_packet(char data[]){
 
@@ -52,165 +45,50 @@ struct data_packet get_packet(char data[]){
     return packet;
 }
 
-// funtion to clear buffer 
-void clearBuf(char* b) 
-{ 
-    int i; 
-    for (i = 0; i < NET_BUF_SIZE; i++) 
-        b[i] = '\0'; 
-} 
-    
-// funtion sending file 
-int sendFile(FILE* fp, char* buf, int s) 
-{ 
-    int i, len; 
-    if (fp == NULL) { 
-        strcpy(buf, nofile); 
-        len = strlen(nofile); 
-        buf[len] = EOF; 
-        return 1; 
-    } 
-  
-    char ch, ch2; 
-    for (i = 0; i < s; i++) { 
-        ch = fgetc(fp); 
-        buf[i] = ch; 
-        if (ch == EOF) 
-            return 1; 
-    } 
-    return 0; 
-} 
-
-
-  
-// driver code 
-int main() 
-{ 
-    int sockfd, nBytes; 
-    struct data_packet packet;
-    struct ack_packet ack;
-
-    struct sockaddr_in addr_con; 
-    int addrlen = sizeof(addr_con); 
-    addr_con.sin_family = AF_INET; 
-    addr_con.sin_port = htons(PORT_NO); 
-    addr_con.sin_addr.s_addr = INADDR_ANY; 
-    char net_buf[sizeof(packet.data)]; 
-    FILE* fp; 
-    printf("size : %d",sizeof(packet.data));
-    // socket() 
-    sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL); 
-  
-    if (sockfd < 0) 
-        printf("\nfile descriptor not received!!\n"); 
-    else
-        printf("\nfile descriptor %d received\n", sockfd); 
-  
-    // bind() 
-    if (bind(sockfd, (struct sockaddr*)&addr_con, sizeof(addr_con)) == 0) 
-        printf("\nSuccessfully binded!\n"); 
-    else
-        printf("\nBinding Failed!\n"); 
-  
-    while (1) { 
-        printf("\nWaiting for file name...\n"); 
-
-       
-        // receive file name 
-        clearBuf(net_buf); 
-  
-        nBytes = recvfrom(sockfd, net_buf, 
-                          NET_BUF_SIZE, sendrecvflag, 
-                          (struct sockaddr*)&addr_con, &addrlen); 
-        
-        //printf("%s      ",net_buf+sizeof(packet));
-        memcpy(&packet.seqno,net_buf ,sizeof(int));
-        memcpy(&packet.sent_time,net_buf +sizeof(int),sizeof(int));
-        memcpy(&packet.data,net_buf + 2*sizeof (int)
-        ,sizeof(packet.data));
-
-        printf(" Seq Num : %d\n",packet.seqno);
-        printf(" Time : %d\n",packet.sent_time);
-        printf("data : %s ",packet.data);
-
-        printf("File Name Received\n");        
-
-        fp = fopen(packet.data, "r"); 
-        printf("\nFile Name Received: %s\n", packet.data); 
-
-
-
-        if (fp == NULL) 
-            printf("\nFile open failed!\n"); 
-        else
-            printf("\nFile Successfully opened!\n"); 
-  
-        int seq_num = 0;
-        while (1) { 
-            seq_num++;
-            //start sending
-            struct data_packet my_packet;
-            char buff_to_send[sizeof(packet)]; 
-            my_packet.sent_time = time(NULL);
-            my_packet.seqno = seq_num;
-            memcpy(buff_to_send,&my_packet.seqno,sizeof(int));
-            memcpy(buff_to_send + sizeof (int)
-                ,&my_packet.sent_time,sizeof(int));
-            
-            // process 
-            if (sendFile(fp, net_buf, NET_BUF_SIZE)) { 
-                sendto(sockfd, net_buf, NET_BUF_SIZE, 
-                       sendrecvflag,  
-                    (struct sockaddr*)&addr_con, addrlen); 
-                break; 
-            } 
-
-            // send 
-            memcpy(buff_to_send + 2 * sizeof(int),net_buf,NET_BUF_SIZE);
-            sendto(sockfd, buff_to_send, sizeof (buff_to_send), 
-                   sendrecvflag, 
-                (struct sockaddr*)&addr_con, addrlen); 
-            clearBuf(net_buf);
-            clearBuf(buff_to_send); 
-            
-            while (1){
-            nBytes = recvfrom(sockfd, net_buf, 
-                          2 * sizeof(int), sendrecvflag, 
-                          (struct sockaddr*)&addr_con, &addrlen);
-            if (nBytes<2 * sizeof(int))
-                continue;
-            memcpy(&ack.seqno,net_buf ,sizeof(int));
-            memcpy(&ack.ackno,net_buf +sizeof(int),sizeof(int));
-            printf("\nAck Received \n");
-            printf("Number : %d\n",ack.ackno);
-
-            clearBuf(net_buf);
-            break;
-            }
-        } 
-        if (fp != NULL) 
-            fclose(fp); 
-        
-    } 
-    return 0; 
-} 
-
-
-
-/*
-
-void stop_and_wait(int sock_fd){
-    char buffer[MAXLEN];
+void start(int sock_fd){
+    char buffer[MAX_LEN];
     int n, client_len;
     struct sockaddr_in client_addr;
     struct data_packet packet;
+    socklen_t socklen = sizeof(struct sockaddr_in);
 
-    memset(&client_addr, 0, sizeof(client_addr));
-    puts("hii");
+    while(1) {
+        memset(&client_addr, 0, sizeof(client_addr));
+        if(recvfrom(sock_fd, (void *) &packet, sizeof(struct data_packet),
+                   0, (struct sockaddr *) &client_addr, &socklen) == -1){
+                        fprintf(stderr, "recvfrom() failed.\n");
+                        return;
+                   }
 
-    n = recvfrom(sock_fd, (char *)buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr *) &client_addr, &client_len);
+        int pid = fork();
+        char *file_name = packet.data;
+        char buffer[500];
 
-    printf("client : %d", n);
+        if (pid == 0) {
+            FILE *file = fopen(file_name, "r");
+            if (file == NULL) {
+                fprintf(stderr, "File not found\n");
+                return;
+            }
+
+            fseek(file, 0L, SEEK_END);
+            long fsize = ftell(file);
+            fseek(file, 0L, SEEK_SET);
+
+            while (fsize && fread(buffer, sizeof(char), min(MAX_LEN, fsize), file) > 0){
+                struct data_packet packet;
+                packet = get_packet(buffer);
+
+                if(sendto(sock_fd, (const void *) &packet, sizeof(struct data_packet), 0,
+                        (struct sockaddr *) &client_addr, socklen) == -1)
+                    {
+                        fprintf(stderr, "sending packet failed.\n");
+                    }
+                fsize -= min(fsize, MAX_LEN);
+                memset(&buffer, 0, sizeof(buffer));
+            }
+        }
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -249,17 +127,7 @@ int main(int argc, char const *argv[])
         puts("bind error");
 
 
-    stop_and_wait(socket_fd);
-    // char buffer[MAXLEN];
-    // int n;
-    // int client_len;
-
-    // n = recvfrom(socket_fd, (char *)buffer, MAXLEN, MSG_WAITALL, (struct sockaddr *) &client_addr, &client_len);
-    // buffer[n] = '\0';
-
-    // printf("client : %s", buffer);
+    start(socket_fd);
 
     return 0;
 }
-
-*/
